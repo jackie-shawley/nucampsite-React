acfe3716051201a6e8b238ecd01187fa312bd1bd
jackie-shawley <jackiekopco@aol.com>
2021-06-26 12:09:59 -0400

    Route Update About Us

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -52,8 +52,7 @@ class Main extends Component {
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
-                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
-                    <Route path='/aboutus/:partnersId' component={About} />
+                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />                    
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
